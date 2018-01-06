a6f64d4d45476669c1c4ef735963b4260a00d5e5
Beauwie <kvn.philippe@gmail.com>
2018-01-06 15:17:59 +0100

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io();
 
 socket.on("connect", function() {
   console.log("Connected to server.");
-
-  socket.emit("createMessage", {
-    from: "User",
-    text: "hey you"
-  });
 });
 
 socket.on("disconnect", function() {
